5a27bfd4b97628c7ab43340c523f22ed13734891
Adarsh Maurya <adarshmaurya0022@gmail.com>
2023-07-25 21:42:00 +0530

    Commit

--- Javascript/main.js
+++ Javascript/main.js
@@ -10,10 +10,10 @@ $(document).ready(function(){
     $(".hamb").on("click", function () {
         $("#navLinks").slideToggle("slow");
         if(i%2 != 0){
-            $(".one").css("transform","rotate(319deg)");
+            $(".one").css("transform","rotate(-41deg)");
             $(".one").css("transformOrigin","right");
             $(".two").css("opacity","0");
-            $(".three").css("transform","rotate(-319deg)");
+            $(".three").css("transform","rotate(41deg)");
             $(".three").css("transformOrigin","right");
             $( ".navbar" ). animate({ marginBottom: "160px" }, 500 );
         }else{
