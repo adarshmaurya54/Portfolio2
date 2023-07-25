e958077a96cb52464628a88bda6e509f23e61aa7
Adarsh Maurya <adarshmaurya0022@gmail.com>
2023-07-25 20:49:59 +0530

    hamburger design

--- Javascript/main.js
+++ Javascript/main.js
@@ -4,3 +4,24 @@ const typed = new Typed('#typed', {
     backSpeed: 150,
     loop: true,
 });
+
+$(document).ready(function(){
+    var i = 1;
+    $(".hamb").on("click", function () {
+        if(i%2 != 0){
+            $(".one").css("transform","rotate(319deg)");
+            $(".one").css("transformOrigin","right");
+            $(".two").css("opacity","0");
+            $(".three").css("transform","rotate(-319deg)");
+            $(".three").css("transformOrigin","right");
+            // $(".three").css("width","94%");
+
+            
+        }else{
+            $(".one").css("transform","rotate(0)");
+            $(".two").css("opacity","1");
+            $(".three").css("transform","rotate(0)");
+        }
+        i++;
+    });
+})
