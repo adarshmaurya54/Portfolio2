e3eaf7f33546a34cb2609bbd8cf376361500a9de
Adarsh Maurya <adarshmaurya0022@gmail.com>
2023-07-27 01:00:10 +0530

    Home page completed, some changes may be occur..

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,26 +1,27 @@
-// const typed = new Typed('#typed', {
-//     strings: ['web developer.', 'software engineer.'],
-//     typeSpeed: 100,
-//     backSpeed: 50,
-//     backDelay: 1500,
-//     loop: true,
-// });
+const typed = new Typed('#typed', {
+    strings: ['web developer.', 'software engineer.'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+});
 
 $(document).ready(function(){
     var i = 1;
     $(".hamb").on("click", function () {
+        $(".mobile-screen-nav").toggle();
         if(i%2 != 0){
             $(".one").css("transform","rotate(-41deg)");
             $(".one").css("transformOrigin","right");
             $(".two").css("opacity","0");
             $(".three").css("transform","rotate(41deg)");
             $(".three").css("transformOrigin","right");
-            $(".container").css("height","100vh");
+            $("body").css("overflow","hidden");
         }else{
             $(".one").css("transform","rotate(0)");
             $(".two").css("opacity","1");
             $(".three").css("transform","rotate(0)");
-            $(".container").css("height","0");
+            $("body").css("overflow","auto");
         }
         i++;
     });
