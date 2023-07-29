cf7b4a05dc6d9ceaa09b7b4fe8ac865499e7420d
Adarsh Maurya <adarshmaurya0022@gmail.com>
2023-07-29 14:35:35 +0530

    making ajax page reloading

--- Javascript/main.js
+++ Javascript/main.js
@@ -22,3 +22,57 @@ $(document).ready(function(){
         i++;
     });
 })
+function ajaxcall(Url) {
+    $.ajax({
+        type: "GET",
+        url: Url,
+        dataType: "html",
+        success: function (response) {
+            $("body").html(response);
+        }
+    });
+}
+$(".hom").on("click",function (e) {
+    e.preventDefault();
+    ajaxcall("./index.html");
+    var firstActiveClass = $(".active");
+    firstActiveClass.addClass("line-bottom");
+    firstActiveClass.removeClass("active");
+    $(".hom").addClass("active");
+    $(".hom").removeClass("line-bottom");
+})
+$(".abo").on("click",function (e) {
+    e.preventDefault();
+    ajaxcall("./about.html");
+    // var firstActiveClass = $(".active");
+    // console.log(firstActiveClass);
+    $(".abo").addClass("active");
+    $(".abo").removeClass("line-bottom");
+})
+$(".res").on("click",function (e) {
+    e.preventDefault();
+    ajaxcall("./resume.html");
+    var firstActiveClass = $(".active");
+    firstActiveClass.addClass("line-bottom");
+    firstActiveClass.removeClass("active");
+    $(".res").addClass("active");
+    $(".res").removeClass("line-bottom");
+})
+$(".serv").on("click",function (e) {
+    e.preventDefault();
+    ajaxcall("./services.html");
+    var firstActiveClass = $(".active");
+    firstActiveClass.addClass("line-bottom");
+    firstActiveClass.removeClass("active");
+    $(".serv").addClass("active");
+    $(".serv").removeClass("line-bottom");
+})
+$(".cont").on("click",function (e) {
+    e.preventDefault();
+    ajaxcall("./contact_us.html");
+    var firstActiveClass = $(".active");
+    firstActiveClass.addClass("line-bottom");
+    firstActiveClass.removeClass("active");
+    $(".cont").addClass("active");
+    $(".cont").removeClass("line-bottom");
+})
--- Javascript/typedjs.js
+++
@@ -1,7 +0,0 @@
-const typed = new Typed('#typed', {
-    strings: ['web developer.', 'software engineer.'],
-    typeSpeed: 100,
-    backSpeed: 50,
-    backDelay: 1500,
-    loop: true,
-});
