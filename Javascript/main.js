4777087dc58dd472c3f73791da4ad9fa309e2413
Adarsh Maurya <adarshmaurya0022@gmail.com>
2023-07-29 11:34:50 +0530

    some changes

---
+++ Javascript/main.js
@@ -0,0 +1,24 @@
+
+
+$(document).ready(function(){
+    var i = 1;
+    $(".hamb").on("click", function () {
+        $(".mobile-screen-nav").toggle();
+        if(i%2 != 0){
+            $(".one").css("transform","rotate(-41deg)");
+            $(".one").css("transformOrigin","right");
+            $(".two").css("opacity","0");
+            $(".three").css("transform","rotate(41deg)");
+            $(".three").css("transformOrigin","right");
+            $("body").css("overflow","hidden");
+            $(".mobile-nav-overlay").css("display","block");
+        }else{
+            $(".one").css("transform","rotate(0)");
+            $(".two").css("opacity","1");
+            $(".three").css("transform","rotate(0)");
+            $("body").css("overflow","auto");
+            $(".mobile-nav-overlay").css("display","none");
+        }
+        i++;
+    });
+})
