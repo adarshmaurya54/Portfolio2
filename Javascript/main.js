0abdf9f3e3bb5cb9becea893288d912677342c93
Adarsh Maurya <adarshmaurya0022@gmail.com>
2023-07-29 17:19:02 +0530

    making responsive navigation bar

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,23 +1,25 @@
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     var i = 1;
     $(".hamb").on("click", function () {
         $(".mobile-screen-nav").toggle();
-        if(i%2 != 0){
-            $(".one").css("transform","rotate(-41deg)");
-            $(".one").css("transformOrigin","right");
-            $(".two").css("opacity","0");
-            $(".three").css("transform","rotate(41deg)");
-            $(".three").css("transformOrigin","right");
-            $("body").css("overflow","hidden");
-            $(".mobile-nav-overlay").css("display","block");
-        }else{
-            $(".one").css("transform","rotate(0)");
-            $(".two").css("opacity","1");
-            $(".three").css("transform","rotate(0)");
-            $("body").css("overflow","auto");
-            $(".mobile-nav-overlay").css("display","none");
+        if (i % 2 != 0) {
+            $(".one").css("transform", "rotate(-41deg)");
+            $(".one").css("transformOrigin", "right");
+            $(".two").css("opacity", "0");
+            $(".three").css("transform", "rotate(41deg)");
+            $(".three").css("transformOrigin", "right");
+            $("body").css("overflow", "hidden");
+            $(".mobile-nav-overlay").css("display", "block");
+            console.log("if");
+        } else {
+            console.log("else");
+            $(".one").css("transform", "rotate(0)");
+            $(".two").css("opacity", "1");
+            $(".three").css("transform", "rotate(0)");
+            $("body").css("overflow", "auto");
+            $(".mobile-nav-overlay").css("display", "none");
         }
         i++;
     });
@@ -28,51 +30,36 @@ function ajaxcall(Url) {
         url: Url,
         dataType: "html",
         success: function (response) {
-            $("body").html(response);
+            // Replace the entire document with the content of the new page
+            document.open();
+            document.write(response);
+            document.close();
         }
     });
 }
-$(".hom").on("click",function (e) {
+$(".hom").on("click", function (e) {
     e.preventDefault();
     ajaxcall("./index.html");
-    var firstActiveClass = $(".active");
-    firstActiveClass.addClass("line-bottom");
-    firstActiveClass.removeClass("active");
-    $(".hom").addClass("active");
-    $(".hom").removeClass("line-bottom");
+    $("body").css("overflow", "auto");
 })
-$(".abo").on("click",function (e) {
+$(".abo").on("click", function (e) {
     e.preventDefault();
     ajaxcall("./about.html");
-    // var firstActiveClass = $(".active");
-    // console.log(firstActiveClass);
-    $(".abo").addClass("active");
-    $(".abo").removeClass("line-bottom");
+    $("body").css("overflow", "auto");
 })
-$(".res").on("click",function (e) {
+$(".res").on("click", function (e) {
     e.preventDefault();
     ajaxcall("./resume.html");
-    var firstActiveClass = $(".active");
-    firstActiveClass.addClass("line-bottom");
-    firstActiveClass.removeClass("active");
-    $(".res").addClass("active");
-    $(".res").removeClass("line-bottom");
+    $("body").css("overflow", "auto");
 })
-$(".serv").on("click",function (e) {
+$(".serv").on("click", function (e) {
     e.preventDefault();
     ajaxcall("./services.html");
-    var firstActiveClass = $(".active");
-    firstActiveClass.addClass("line-bottom");
-    firstActiveClass.removeClass("active");
-    $(".serv").addClass("active");
-    $(".serv").removeClass("line-bottom");
+    $("body").css("overflow", "auto");
 })
-$(".cont").on("click",function (e) {
+$(".cont").on("click", function (e) {
     e.preventDefault();
     ajaxcall("./contact_us.html");
-    var firstActiveClass = $(".active");
-    firstActiveClass.addClass("line-bottom");
-    firstActiveClass.removeClass("active");
-    $(".cont").addClass("active");
-    $(".cont").removeClass("line-bottom");
+    $("body").css("overflow", "auto");
 })
+
