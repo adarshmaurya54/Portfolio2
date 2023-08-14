14cfbf1d83e7fd6d4fed316d5ad3a843a9852aa6
Adarsh Maurya <adarshmaurya0022@gmail.com>
2023-08-14 21:01:17 +0530

    some changes

--- Javascript/other.js
+++ Javascript/other.js
@@ -1,8 +1,8 @@
+
 var independenceDayDate = "1947/08/13";
 function calculateYear(date) { // birthday is a date
     // Below one is the single line logic to calculate the no. of years...
     var years = new Date(new Date() - new Date(date)).getFullYear() - 1970;
-    console.log(years);
     return years;
 }
 const targetDate = new Date('2023/8/15').getTime();
@@ -14,35 +14,45 @@ function updateCountdown() {
     if (timeRemaining <= 0) {
         countdownElement.innerHTML = ``;
         clearInterval(interval);
-    } 
+    }
     else {
-            const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-            const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-    
+
         countdownElement.textContent = `${days}d ${hours}h ${minutes}m ${seconds}s`;
     }
-    console.log("hello");
 }
 const interval = setInterval(updateCountdown, 1000);
+let audio;
 function checkdate() {
     let date = new Date();
     let todayDate = date.getDate();
     let currentMonth = date.getMonth() + 1;
     if ((todayDate == 14 || todayDate == 15) && currentMonth == 8) {
-        
+
         $(".other-msgs").css("display", "flex");
         $(".message").html(`<p>HAPPY ${calculateYear(independenceDayDate) + 1}th INDEPENDENCE DAY</p>`);
         $(".fa-close").on("click", function () {
             $(".other-msgs").css("display", "none");
+            audio.remove();
         })
         $(".container").css("background", "linear-gradient(140deg,#FF993371 , #fafafa71,#13880871)")
         $(".navbar").css("background", "linear-gradient(140deg,#FF993371 , #fafafa71,#13880871)")
         $("footer").css("background", "linear-gradient(140deg,#FF993371 , #fafafa71,#13880871)")
         $(".mobile-screen-nav").css("background", "linear-gradient(140deg,#FF993371 , #fafafa71,#13880871)");
-        let audio = new Audio("../musics/1st.mp3");
-        audio.play();
-    } 
-} 
+        // audio =  new Audio("../musics/1st.mp3");
+        // audio.play();
+        var audio = document.createElement(`iframe`)
+        document.body.appendChild(audio);
+        audio.src = "../musics/1st.mp3"
+        audio.setAttribute("allow", "autoplay");
+
+        // audio.play(); 
+    }
+}
+function playSound() {
+}
+playSound()
 checkdate();
